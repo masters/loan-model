49d32895172d3baf080d1252125a1561390e6d5b
roar109 <roar109@gmail.com>
2014-09-29 11:05:09 -0500

    Adds parent pom and add person properties

--- src/main/java/org/rage/loan/model/Person.java
+++ src/main/java/org/rage/loan/model/Person.java
@@ -5,11 +5,19 @@ import java.io.Serializable;
 public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private String id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String relation;
 	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
